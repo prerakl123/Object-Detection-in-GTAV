dad54848875b690983bbbb1e9bb4915a6a883789
Prerak <prerakl123@gmail.com>
2023-10-05 02:24:31 +0530

    Add keys.py for ASCII values of keys.

---
+++ keys.py
@@ -0,0 +1,33 @@
+# Command keys
+BKSPACE = 8
+RETURN = 10
+LSHIFT = 0xFFE1
+RSHIFT = 0xFFE2
+
+SPACE = 32
+
+# For keys 0-9
+NUM_ALL = range(48, 58)
+
+# All number keys
+NUM_0 = 48
+NUM_1 = 49
+NUM_2 = 50
+NUM_3 = 51
+NUM_4 = 52
+NUM_5 = 53
+NUM_6 = 54
+NUM_7 = 55
+NUM_8 = 56
+NUM_9 = 57
+
+# For keys A-Z
+CHAR_CAPS_ALL = range(65, 91)
+# For keys a-z
+CHAR_LOWERCASE_ALL = range(97, 123)
+
+# Arrow Keys
+ARROW_LEFT = 81
+ARROW_UP = 82
+ARROW_RIGHT = 83
+ARROW_DOWN = 84
